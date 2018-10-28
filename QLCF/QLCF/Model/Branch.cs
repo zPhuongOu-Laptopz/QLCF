ace7800c5988b67ac7eefea76829537c3ff15413
Nguyễn Ngọc Phương <34095199+zPhuongOu-PCz@users.noreply.github.com>
2018-10-28 15:15:41 +0700

    Insert. Update, Delete

--- QLCF/QLCF/Model/Branch.cs
+++ QLCF/QLCF/Model/Branch.cs
@@ -80,5 +80,44 @@ namespace QLCF.Model
                 cn.Close();
             }
         }
+
+        public void Insert()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["cStr"].ConnectionString;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                string query = "INSERT INTO [dbo].[Branch] VALUES (" + this.branchName + "," + this.address + "," + this.eNumber + "," + this.eNumberExpect + ")";
+
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["cStr"].ConnectionString;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                string query = "UPDATE [dbo].[Branch] SET [branchName] = " + this.branchName + ",[address] = " + this.address + ",[eNumber] = " + this.eNumber + ",[eNumberExpect] = " + this.eNumberExpect + "WHERE id = " + this.id;
+
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["cStr"].ConnectionString;
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                string query = "DELETE FROM [dbo].[Branch] WHERE id = " + this.id;
+
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
